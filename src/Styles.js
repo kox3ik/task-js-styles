f07cf61e168e4a0e6a3fb4a9db1992b85d344d28
Daniil <ivankov.62@list.ru>
2022-12-27 23:17:44 +0300

    gg

--- src/Styles.js
+++ src/Styles.js
@@ -5,16 +5,23 @@
    removeClass(elem, elemСlass),
    toggleClass(elem, elemСlass),
 которые позволяют проверить, есть ли у элемента заданный CSS-класс, добавить к нему CSS-класс, удалить CSS-класс и переключить CSS-класс.
-
 В функции передаются 2 параметра:
    elem - DOM-элемент, который проверяется,
    elemСlass - название класса, которое проверяется.
 */
 
-export function hasClass(elem, elemClass) {}
+export function hasClass(elem, elemClass) {
+    return elem.classList.contains(elemClass);
+}
 
-export function addClass(elem, elemClass) {}
+export function addClass(elem, elemClass) {
+    elem.classList.add(elemClass);
+}
 
-export function removeClass(elem, elemClass) {}
+export function removeClass(elem, elemClass) {
+    elem.classList.remove(elemClass);
+}
 
-export function toggleClass(elem, elemClass) {}
+export function toggleClass(elem, elemClass) {
+    elem.classList.toggle(elemClass);
+}
